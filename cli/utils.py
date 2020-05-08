002b39a51a5cff5f0d0c38ea7709617164b70d5e
Edward Visel <alistaire@gmail.com>
2020-05-08 13:11:35 -0500

    move comments to docs and rearrange a little

--- cli/utils.py
+++ cli/utils.py
@@ -1,13 +1,17 @@
 def _format_host(host: str) -> str: 
-    return host + "/" if host[-1] != "/" else host 
+    if host[-1] != "/":
+        host += "/"
+        
+    return host
 
 
 def _build_pkg_url(host: str, category: str, pkg_name: str) -> str: 
+    """
+    Assemble docs URL for package
+    
+    :param host: e.g. https://docs.your_company.com/ 
+    :param category: typically language, e.g. Python or R 
+    :param pkg_name: package name 
+    """
     # return f'{host}static/{category}/{pkg_name}/index.html'
-    return (
-        _format_host(host)          # e.g. https://docs.your_company.com/ 
-        + 'static/'                 # updoc format 
-        + category.title() + '/'    # typically language, e.g. Python or R 
-        + pkg_name + '/'            # package name 
-        + 'index.html'              # updoc format, doc's index.html is entry pt 
-    )
+    return f"{_format_host(host)}static/{category.title()}/{pkg_name}/index.html'
